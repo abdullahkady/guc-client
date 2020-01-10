957ace5ab87369a4748fc825f8ca5040e96eb24e
Abdullah Kady <abdullahkady@gmail.com>
2020-01-10 23:13:09 +0200

    Update example

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+import puppeteer from 'puppeteer';
 import { LOGIN } from './constants';
 import { getTranscript } from './transcript';
+import { getGrades } from './grades';
 import { writeFileSync } from 'fs';
 
-getTranscript(LOGIN).then(val => {
-  writeFileSync('./output/transcript.json', JSON.stringify(val, null, 2));
-});
+const main = async (): Promise<void> => {
+  const browser = await puppeteer.launch({ headless: false });
+
+  await Promise.all([
+    getTranscript(LOGIN, browser).then(val => {
+      writeFileSync('./output/transcript.json', JSON.stringify(val, null, 2));
+    }),
+    getGrades(LOGIN, browser).then(val => {
+      writeFileSync('./output/grades.json', JSON.stringify(val, null, 2));
+    })
+  ]);
+  await browser.close();
+  console.log('Done, thank you :)');
+};
+
+main();
